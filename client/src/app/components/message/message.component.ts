6efcbc88b92cbdb962c4c0e18e50d2419af9be94
Ben Vogler <ben.vogler@redargyle.com>
2018-09-20 19:06:06 -0400

    Fixed beforeInit privacy

--- client/src/app/components/message/message.component.ts
+++ client/src/app/components/message/message.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-    private beforeInit = true;
+    beforeInit = true;
 
     @Input()
     visible = false;
